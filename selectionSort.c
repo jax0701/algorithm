12d4eb3576e01ed1beeff11493380e92600cc16b
Jax <jargalsaikhan.n@gmail.com>
2015-08-23 22:13:46 -0400

    Revised selectionSort

--- selectionSort.c
+++ selectionSort.c
@@ -4,21 +4,21 @@ void main(void){
    int alist[]={4,6,7,2,3,44,12,100,1,32,34,1523};
    int positionOfMax, temp, location, i;
    int length=sizeof(alist)/sizeof(alist[0]);
-   int index=length-1;
+   int fillSlot=length-1;
    
    do{
 	   positionOfMax=0;
-	   for(location=1;location<=index; location++){
+	   for(location=1;location<=fillSlot; location++){
 		   if (alist[location]>alist[positionOfMax]){
 			   positionOfMax=location;
 			   }
 	   }
-	   temp=alist[index];
-	   alist[index]=alist[positionOfMax];
+	   temp=alist[fillSlot];
+	   alist[fillSlot]=alist[positionOfMax];
 	   alist[positionOfMax]=temp;
 
-           index--;
-      }while(index>=0);  
+           fillSlot--;
+      }while(fillSlot>=0);  
    
    for(i=0; i<=(length-1);i++){
 	   printf("%d ",alist[i]);} 
