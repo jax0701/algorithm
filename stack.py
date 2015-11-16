9cc17b68afdb820843537c0aaee05c6ef9f4f677
Jax <jargalsaikhan.n@gmail.com>
2015-11-16 03:45:14 -0500

    Adding Stack

---
+++ stack.py
@@ -0,0 +1,60 @@
+"""Notice that the definition of the Stack class is imported from the pythonds module."""
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+class top_begining_Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.insert(0,item)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+
+
+
+s = Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
+
+
+a = top_begining_Stack()
+a.push('hello')
+a.push('true')
+print(a.pop())
