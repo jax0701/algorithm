10b7f790b064910550a09b596d8c17f165d4451d
Jax <jargalsaikhan.n@gmail.com>
2015-12-03 21:15:36 -0500

    Adding hotpotato

---
+++ hotpotato.py
@@ -0,0 +1,20 @@
+from queue import Queue
+
+def hotPotato(namelist, num):
+	simqueue = Queue()
+	for name in namelist:
+		simqueue.enqueue(name)
+        
+	while len(simqueue.items)>1:
+		for n in range(num):
+			if n < (num-1):
+				item = simqueue.dequeue()
+		        	simqueue.enqueue(item)
+			else:
+				simqueue.dequeue()
+
+        print simqueue.items
+
+namelist = ['Jax', 'Brain', 'Edgar', 'James', 'Eddie', 'David', 'Joe', 'Victor', 'Paul', 'Alice', 'Jinah', 'Ryan', 'Jason', 'Nolla', 'Olivia', 'Austin', 'Chelsy']
+
+hotPotato(namelist,11)
